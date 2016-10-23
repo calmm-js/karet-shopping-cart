91b116c45f0dafb33c9c2feff2801fc902aafb21
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-10-23 19:19:22 +0300

    Simplified

--- src/components/cart-item.js
+++ src/components/cart-item.js
@@ -4,14 +4,13 @@ import React     from "karet"
 import Counter from "./counter"
 import Remove  from "./remove"
 
-export const name = "name"
-export const count =
+const cartCount =
   P(L.choose(({count: _, ...rest} = {}) => L.defaults({count: 0, ...rest})),
     "count")
 
 export default ({item}) =>
   <li>
     <Remove removable={item}/>
-    <Counter count={item.lens(count)}/>
-    {item.view(name)}
+    <Counter count={item.lens(cartCount)}/>
+    {item.view("name")}
   </li>
--- src/components/product-item.js
+++ src/components/product-item.js
@@ -1,19 +1,20 @@
-import * as R             from "ramda"
-import K                  from "karet.util"
-import P, * as L          from "partial.lenses"
-import React, {fromKefir} from "karet"
+import * as R    from "ramda"
+import K         from "karet.util"
+import P, * as L from "partial.lenses"
+import React     from "karet"
 
 import Counter from "./counter"
 
-const count = item =>
+const productCount = item =>
   P(L.find(R.whereEq({id: item.id})),
     L.defaults(item),
     "count",
     L.defaults(0),
     L.normalize(R.max(0)))
 
-export default cart => ({item}) => fromKefir(K(item, item =>
+export default cart => ({item}) =>
   <li>
-    <Counter count={cart.lens(count(item))}/>
-    {item.name}
-  </li>))
+    {K(item, item =>
+       <Counter count={cart.lens(productCount(item))}/>)}
+    {item.view("name")}
+  </li>
