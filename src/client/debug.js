b453966a3e9bae8a4c3191111aa2a8f89a2ad987
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-10-25 20:07:15 +0300

    More useful debug "exports"

--- src/client/debug.js
+++ src/client/debug.js
@@ -1,8 +1,10 @@
+import K, * as U from "karet.util"
+import P, * as L from "partial.lenses"
+
 import {state} from "./start"
 
-import * as L from "partial.lenses"
-import Atom   from "kefir.atom"
-
-window.Atom = Atom
+window.K = K
 window.L = L
+window.P = P
+window.U = U
 window.state = state
