0572ce6feb3c8ea71adfaea1b95485edaed60df3
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-10-25 20:06:49 +0300

    Unused

--- src/client/window.js
+++
@@ -1,8 +0,0 @@
-import * as Kefir from "kefir"
-
-export const innerWidth =
-  Kefir.fromEvents(window, "resize")
-  .merge(Kefir.constant(0))
-  .toProperty()
-  .map(() => window.innerWidth)
-  .skipDuplicates()
