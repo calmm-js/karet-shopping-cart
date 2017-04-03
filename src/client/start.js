22551a0bbd678ea9021db004eb4d10270756148a
Vesa Karvonen <vesa.a.j.k@gmail.com>
2017-04-03 09:54:53 +0300

    Preferred style

--- src/client/start.js
+++ src/client/start.js
@@ -1,9 +1,9 @@
 import * as U   from "karet.util"
 import React    from "karet"
-import ReactDOM from "react-dom"
+import {render} from "react-dom"
 
 import App from "../components/app"
 
 export const state = U.atom({})
 
-ReactDOM.render(<App {...{state}}/>, document.getElementById("app"))
+render(<App state={state}/>, document.getElementById("app"))
